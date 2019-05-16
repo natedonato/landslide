5f56e74be9c1eafb95cc0152c075e07a11badbca
Nate Donato <natedonato@gmail.com>
2019-05-16 11:57:47 -0700

    adds time based game loop with requestAnimationFrame, guy can move with left and right arrow keys with velocity and friction

--- landslide.js
+++ landslide.js
@@ -1,46 +1,133 @@
+
 const canvas = document.getElementById("game");
     canvas.width = 640;
     canvas.height = 500;
     ctx = canvas.getContext("2d");
-    ctx.fillStyle = '#FFA500';
+    
+
+
+    ///new game setup and properties
+    let bgcolor = '#FFA500';
+    
+    let playercolor = "#ff1919";
+    let initialX = canvas.width / 2;
+    let initialY = canvas.height - 50;
+    let guyspeed = 10;
+    let friction = 0.85;
+    let gravity = 0.3;
+    let keys = {};
+
+    ctx.fillStyle = bgcolor;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = '#000000';
 
-
-    
-    let initialX = canvas.width / 2;
-    let initialY = canvas.height - 50;
-
     let guy = {
-        x: initialX,
-        y: initialY,
+        pos: {
+            x: initialX,
+            y: initialY
+        },
+        vel: {
+            x: 1,
+            y: 0,
+        },
         width: 22,
         height: 32,
+        maxspeed: guyspeed,
+        airborne: true,
     };
 
 
-    const update = () => {
-
-    ctx.clearRect(guy.x, guy.y, guy.width, guy.height);
-
-    guy.x += 2;
-    ctx.fillStyle = "#ff1919";
-    ctx.fillRect(guy.x, guy.y, guy.width, guy.height);
-        setTimeout(update);
-    }
 
 
 
 
-window.addEventListener("load", function () {
-    setInterval( update, 200);
+
+
+
+
+
+
+
+
+
+    const drawGuy = () => {
+        ctx.fillStyle = "#ff1919";
+        ctx.fillRect(guy.pos.x, guy.pos.y, guy.width, guy.height);
+
+    };
+
+    const updateGuyPos = (dt) => {
+        if(isNaN(dt)){
+            dt = 1;
+        }
+        handleKeys();
+
+        if(guy.pos.x > canvas.width - guy.width / 2)
+        {guy.pos.x = -4;}
+        else if 
+        (guy.pos.x < -5){guy.pos.x = canvas.width - guy.width / 2 - 2;}
+
+        guy.pos.x += guy.vel.x * dt;
+    };
+
+
+
+    //39 = right arrow,  37 = left arrow
+
+    const handleKeys = () => {
+        if(keys[39]){
+            if(guy.vel.x < guy.maxspeed){
+                guy.vel.x += 1;
+            }
+        }
+
+        if(keys[37]){
+            if(guy.vel.x > -guy.maxspeed){
+                guy.vel.x -= 1;
+            }
+        }
+        
+        guy.vel.x *= friction;
+    };
+
+
+
+    //setting up some vars for main loop 
+    let end = 0;
+    let dt = 0;
+
+    //main loop of game
+    const update = (timestamp) => {       
+        dt = (timestamp - end) / 10;
+        ctx.fillStyle = bgcolor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = '#000000';
+
+        updateGuyPos(dt);
+        drawGuy();
+        end = timestamp;
+
+        requestAnimationFrame(update);
+    };
+
+
+
+
+
+window.addEventListener("keydown", function(e) {
+    e.preventDefault();
+    keys[e.keyCode] = true;
+    console.log(keys);
+});
+
+window.addEventListener("keyup", function(e) {
+    keys[e.keyCode] = false;
+    console.log(keys);
 });
 
 
 
 
-
-
-
-
-
+window.addEventListener("load", function () {
+    update();
+});
