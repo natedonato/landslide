f1392e6185f85b34da618fa8d1d4c6eadf31a713
Nate Donato <natedonato@gmail.com>
2019-06-13 10:56:19 -0700

    secret key work on local but not deploy

--- routes/api/scores.js
+++ routes/api/scores.js
@@ -14,6 +14,7 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     if(req.body.secretKey != secretKey){
+        console.log(req.body.secretKey);
         return res.status(403).json({score: "Please do not post fake scores, this is a personal project being shown to potential employers!"});
     }
     Score.findOne({score: req.body.score, name: req.body.name})
